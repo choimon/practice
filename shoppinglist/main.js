d636320e2f61fb8429e9b02de150c0775634fb0d
Minjeong Choi <sanjosebadger@gmail.com>
2020-08-18 02:47:10 +0900

    Using an event delegation for addeventlisteners

--- shoppinglist/main.js
+++ shoppinglist/main.js
@@ -19,49 +19,53 @@ function onAdd(itemStr) {
   newLi.scrollIntoView({ block: 'center' });
   return;
 }
-function createItem(text) {
-  // const newLi = document.createElement('li');
-  // newLi.setAttribute('class', 'item-row');
-  // newLi.innerHTML = `
-  //   <div class="item">
-  //     <span class="item-text" onClick="toggleStrike(this)">${itemStr}</span>
-  //     <button class="item-delete-icon" onClick="deleteItem(this)">
-  //       <i class="fas fa-trash-alt "></i>
-  //     </button>
-  //   </div>
-  //   <div class="item-divider"></div>
-  //   `;
-  // <span class="item-text" onClick="toggleStrike(this)">${itemStr}</span>
-  // <i class="fas fa-trash-alt item-delete-icon" onClick="deleteItem(this)"></i>
 
+// let id = 0;
+let id = items.length; // for existing items
+function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item-row');
+  itemRow.setAttribute('data-id', id);
 
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
+  // if i put data-id to only i, then clicking a space bt i and the button doesn't result in removing inside the event listener
+  itemRow.innerHTML = `
+      <div class="item" data-id=${id}>
+        <span class="item-text" data-id=${id}>${text}</span>
+        <button class="item-delete-icon" data-id=${id}>
+          <i class="fas fa-trash-alt" data-id=${id}></i>
+        </button>
+      </div>
+      <div class="item-divider"></div>
+  `;
+  id++;
 
-  const name = document.createElement('span');
-  name.setAttribute('class', 'item-text');
-  name.innerHTML = text;
-  item.addEventListener('click', () => {
-    toggleStrike(name);
-  });
+  // const itemRow = document.createElement('li');
+  // itemRow.setAttribute('class', 'item-row');
+  // const item = document.createElement('div');
+  // item.setAttribute('class', 'item');
 
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item-delete-icon');
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  deleteBtn.addEventListener('click', () => {
-    itemList.removeChild(itemRow);
-  });
+  // const name = document.createElement('span');
+  // name.setAttribute('class', 'item-text');
+  // name.innerHTML = text;
+  // item.addEventListener('click', () => {    //////
+  //   toggleStrike(name);
+  // });
 
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item-divider');
+  // const deleteBtn = document.createElement('button');
+  // deleteBtn.setAttribute('class', 'item-delete-icon');
+  // deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  // deleteBtn.addEventListener('click', () => {
+  //   itemList.removeChild(itemRow);
+  // });
 
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
+  // const itemDivider = document.createElement('div');
+  // itemDivider.setAttribute('class', 'item-divider');
 
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  // item.appendChild(name);
+  // item.appendChild(deleteBtn);
+
+  // itemRow.appendChild(item);
+  // itemRow.appendChild(itemDivider);
   return itemRow;
 }
 
@@ -84,6 +88,7 @@ addIcon.addEventListener('click', () => {
 });
 
 // initializing for existing ones
+/*
 items.forEach((item) => {
   item.addEventListener('click', () => {
     // https://stackoverflow.com/questions/18880890/how-do-i-toggle-an-elements-class-in-pure-javascript
@@ -97,4 +102,29 @@ trashIcons.forEach((item) => {
     // console.log(this); //window
     deleteItem(item);
   });
+});
+*/
+
+// using event delegation
+itemList.addEventListener('click', (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const targetNode = event.target.nodeName;
+    if (targetNode === 'I' || targetNode === 'BUTTON') {
+      // if more than one button/I :
+      // const className = 'fas fa-trash-alt'
+      // if (event.target.className === 'fas fa-trash-alt')
+      const toBeDeleted = document.querySelector(`.item-row[data-id="${id}"]`);
+      toBeDeleted.remove();
+    } else {
+      // const name = document.createElement('span');
+      // item.addEventListener('click', () => {    //////
+      //   toggleStrike(name);
+      // });
+      const toBeToggled = document.querySelector(
+        `.item-row[data-id="${id}"] .item-text`
+      );
+      toggleStrike(toBeToggled);
+    }
+  }
 });
